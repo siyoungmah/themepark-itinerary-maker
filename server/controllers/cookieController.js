007097cb2cfb324a61cad861432598aec8bef107
Si Young Mah <siyoungmah@gmail.com>
2022-08-11 18:01:15 -0700

    Streamline redirect to new-user process
    If the user does not have a cookie, immediately redirect to
    /new-user instead of moving along the middleware train

--- server/controllers/cookieController.js
+++ server/controllers/cookieController.js
@@ -13,14 +13,13 @@ cookieController.verifyReturningUser = (req, res, next) => {
       .exec((err, user) => {
         if (err) return next('Error in cookieController.verifyReturningUser: ' + JSON.stringify(err));
         if (user) res.locals.verifiedUser = true;
-        else res.locals.verifiedUser = false;
         return next();
       })
   }
   else {
-    // if ssid did not exits
+    // if ssid did not exits, redirect to /new-user to assign cookie
     res.locals.verifiedUser = false;
-    return next();
+    return res.status(204).redirect('new-user');
   }
 }
 
--- server/routes/db.js
+++ server/routes/db.js
@@ -17,25 +17,23 @@ const createErr = (errInfo) => {
 
 router.get('/',
   (req, res) => {
-    return res.status(200).json({message: 'db router is working!'});
-});
+    return res.status(200).json({ message: 'db router is working!' });
+  });
 
 // verification that this is indeed a new-user is needed!
-router.get('/new-user', 
+router.get('/new-user',
   dbController.findLastNum,
   dbController.createUser,
   cookieController.setSSIDCookie,
   (req, res) => {
     return res.status(200).json('You should have a cookie!');
-})
+  })
 
 // load saved itineraries if the user already has a cookie
 router.get('/load',
   cookieController.verifyReturningUser,
   (req, res) => {
-    if(res.locals.verifiedUser)
-      return res.status(200).json('Hey, we recognize you! You should have a saved itinerary loading... sooon....');
-    else return res.status(204).redirect('new-user');
+    return res.status(200).json('Hey, we recognize you! You should have a saved itinerary loading... sooon....');
   }
 )
 
